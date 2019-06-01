205df37b888d2331d2c44a92ccd4526f3b892d41
Jonathan Funk <jonfunk88@hotmail.com>
2019-06-01 14:42:24 -0700

    changed port

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
